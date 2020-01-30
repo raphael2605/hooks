f2183652996af7aa33a0be7672356c261003566b
Raphael <raphael@fuerzastudio.com.br>
2020-01-30 16:57:07 -0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <h1>Hello world</h1>;
+  const [tech, setTech] = useState(['ReactJs', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  // useEffect(() => {
+  //   const storageTech = localStorage.getItem('tech');
+
+  //   if (storageTech) {
+  //     setTech(JSON.parse(storageTech));
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias </strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
